9c5fc9ba78a8d606dc0183efb0d849120507058c
kimdohyeong <do5568@naver.com>
2021-01-27 17:19:09 +0900

    Tutorial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,75 @@
 <template>
     <div>
-        <h1>Welcome to Home!</h1>
+        <h1>Welcome to {{title}}!</h1>
+        <input type="text" v-model="input1" />
+        <button type="button" @click="getData">Get</button>
+        <button type="button" @click="setData">Set</button>
+
+        <select class="form-control" v-model="region" @change="changeRegion">
+            <option :key="i" :value="d.v" v-for="(d,i) in options">{{d.t}}</option>
+        </select>
+        
+        <!-- v-show or v-if  -->
+        <table class="table table-bordered" v-show="tableShow">
+            <tr :key="i" v-for="(d,i) in options">
+                <td>{{d.v}}</td>
+                <td>{{d.t}}</td>
+            </tr>
+        </table>
+
     </div>
 </template>
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            title: "첫번째 프로젝트",
+            title2: "두번째 프로젝트",
+            input1: "abcd",
+            options: [
+                {v: "S", t:"Seoul"},
+                {v: "J", t: "Jeju"},
+                {v: "B", t: "Busan"},
+            ],
+            region : "B",
+            tableShow: true,
+        };
+    },
+    methods: {
+        getData() {
+            alert(this.input1);
+        },
+        setData() {
+            this.input1 = "12345" 
+        },
+        changeRegion() {
+            alert(this.region);
+        },
+    },
+    beforeCreate() {
+        console.log("beforeCreate") 
+    },
+    created() {
+        console.log("created")
+    },
+    beforeMount() {
+        console.log("beforeMount")
+    },
+    mounted() {
+        console.log("mounted")
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate")
+    },
+    updated() {
+        console.log("updated")
+    },
+    beforeDestroy() {
+        console.log("beforeDestroy")
+    },
+    destroyed() {
+        console.log("destroyed")
+    },
+
+}
 </script>
